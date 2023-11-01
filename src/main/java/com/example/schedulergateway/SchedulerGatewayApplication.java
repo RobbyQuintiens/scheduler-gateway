ab989267ae14dd8b56a5f38f4bcb1fc65ea3b0a0
RobbyQuintiensPXL <robby.quintiens@student.pxl.be>
2023-11-01 19:35:00 +0100

    gateway

---
+++ src/main/java/com/example/schedulergateway/SchedulerGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.schedulergateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulerGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/schedulergateway/SchedulerGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schedulergateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
